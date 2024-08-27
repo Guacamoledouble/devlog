7eefed3292a61fa6c0514aae303b9743c666c62e
HoejunKwon <86822630+Guacamoledouble@users.noreply.github.com>
2024-08-27 14:07:05 +0900

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,31 +1,31 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "Hoejun Kwon",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    role: "Clinical Statistician & AI builder",
+    bio: "I research and study cancer.",
+    email: "hoejunkwon96@gmail.com",
+    linkedin: "hoejunkwon",
+    github: "Guacamoledouble",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
+      name: `vegabond-coder`,
       href: "https://github.com/Guacamoledouble/devlog",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "vegabond-coder",
     description: "welcome to morethan-log!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
+  link: "https://vagabond-coder.netlify.app/",
+  since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
